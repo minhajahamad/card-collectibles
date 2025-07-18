6a981bb483baec21e1f9b076108de19426e3fa3e
Minhaj Ahamad <minhajahamadmk@gmail.com>
2025-07-18 18:02:28 +0530

    .

--- src/Pages/Landin/landin.jsx
+++ src/Pages/Landin/landin.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import LandinHero from '../../Components/Landin-Hero/landinHero';
 import LandinJoin from '../../Components/Landin-Join/landinJoin';
 import Footer from '../../Components/Footer/footer';
+
 const Landin = () => {
+  const joinRef = useRef(null);
+
   return (
     <div>
-      <LandinHero />
-      <LandinJoin />
+      <LandinHero
+        onLearnMoreClick={() =>
+          joinRef.current?.scrollIntoView({ behavior: 'smooth' })
+        }
+      />
+      <div ref={joinRef}>
+        <LandinJoin />
+      </div>
       <Footer />
     </div>
   );
