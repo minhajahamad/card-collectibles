6b78d152c9a516ac031cb8759f95fbce59f2ed13
Minhaj Ahamad <minhajahamadmk@gmail.com>
2025-07-21 10:16:52 +0530

    modal updated

--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -7,25 +7,27 @@ import { API_URL } from '../../services/api_url';
 
 const NavBar = ({ onLoginClick }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const navigate = useNavigate()
-  const [user,setUser] = useState({})
-  const uuid = localStorage.getItem("uuid")
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const uuid = localStorage.getItem('uuid');
   console.log(uuid);
-  
-  const fetchUser = async()=>{
-    try{
-      const response = await axiosInstance.get(API_URL.USER.GET_USER_UUID(uuid));
+
+  const fetchUser = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_URL.USER.GET_USER_UUID(uuid)
+      );
       console.log(response);
-      setUser(response.data.data)
-    }catch(err){
+      setUser(response.data.data);
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUser();
-  },[])
-  
+  }, []);
+
   return (
     <>
       <nav className="w-full h-[70px] lg:h-[85px]   flex items-center bg-[#09778E]  justify-between px-4 md:px-15 lg:px-20  ">
@@ -35,18 +37,18 @@ const NavBar = ({ onLoginClick }) => {
             className="  object-cover w-[115px] h-[50px] lg:w-[158px] lg:h-[70px] "
           />
         </div>
-        
-        <div
-          className=" flex gap-3 items-center justifu-center text-white text-[24px] lg:text-[30px] cursor-pointer"
-          onClick={() => setSidebarOpen(true)}
-        >
-            <div
-          onClick={onLoginClick}
-          className="bg-white rounded-[6px] text-[#00A397] text-[16px] font-montserrat px-3 py-2 font-semibold cursor-pointer"
-        >
-          <p>SignUp</p>
-        </div>
-          <RxHamburgerMenu className='xl:hidden'/>
+
+        <div className=" flex gap-3 items-center justifu-center text-white text-[24px] lg:text-[30px] cursor-pointer">
+          <div
+            onClick={onLoginClick}
+            className="bg-white rounded-[6px] text-[#00A397] text-[16px] font-montserrat px-3 py-2 font-semibold cursor-pointer"
+          >
+            <p>SignUp</p>
+          </div>
+          <RxHamburgerMenu
+            onClick={() => setSidebarOpen(true)}
+            className="xl:hidden"
+          />
         </div>
 
         {/* <div className=" lg:w-[105px] xl:w-[115px] h-full xl:flex items-center justify-between hidden ">
